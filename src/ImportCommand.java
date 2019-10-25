296b0ed9ec2a59414562974aeecb62e6e093fd69
likholat <aniala201398@gmail.com>
2019-10-25 17:40:51 +0300

    ready

--- src/ImportCommand.java
+++ src/ImportCommand.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class ImportCommand implements RoverCommand {
     private List<RoverCommand> roverCommandList;
     private String filename;
-    Rover rover;
+    private Rover rover;
 
     @Override
     public void execute() {
@@ -27,7 +27,7 @@ public class ImportCommand implements RoverCommand {
         }
     }
 
-    public ImportCommand(String filename, Rover rover){
+    ImportCommand(String filename, Rover rover){
         roverCommandList = new ArrayList<>();
         this.filename = filename;
         this.rover = rover;
